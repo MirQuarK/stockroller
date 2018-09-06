08055674b76fc830728cd9b96b396cb3f58d1539
chengzuan <chengzuan@toucht.net>
2018-09-06 18:57:09 +0800

    timer

--- chz-server/src/main/java/com/hzxc/chz/server/Booter.java
+++ chz-server/src/main/java/com/hzxc/chz/server/Booter.java
@@ -1,9 +1,17 @@
 package com.hzxc.chz.server;
 
+import com.alibaba.fastjson.JSONObject;
+import com.hzxc.chz.dao.GxqUserRepository;
+import com.hzxc.chz.entity.GxqUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,11 +21,30 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableJpaRepositories(basePackages = "com.hzxc.chz.dao")
 @EntityScan(basePackages = "com.hzxc.chz.entity")
 public class Booter {
-    
+
+	private static final Logger logger = LoggerFactory.getLogger(Booter.class);
+
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Booter.class) ;
 		app.addInitializers(new ApplicationStartup());
 		app.run(args);
 	}
-	
+
+	@Bean
+	@Order(3)
+	CommandLineRunner demo(GxqUserRepository repository) {
+		return (args) -> {
+			logger.info("in demo");
+			for(GxqUser gxqUser:repository.findAll()) {
+				logger.info(JSONObject.toJSONString(gxqUser));
+			}
+		};
+	}
+
+	@Bean
+	@Order(8)
+	Object aa() {
+		logger.info("in aa");
+		return null;
+	}
 }
--- chz-server/src/main/java/com/hzxc/chz/server/utils/TimeUtil.java
+++ chz-server/src/main/java/com/hzxc/chz/server/utils/TimeUtil.java
@@ -1,5 +1,9 @@
 package com.hzxc.chz.server.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * create by chz on 2018/3/5
  */
@@ -11,5 +15,67 @@ public class TimeUtil {
         return (hour < 10 ? "0" + hour : hour) + ":"
                 + (min < 10 ? "0" + min : min) + ":"
                 + (sec < 10 ? "0" + sec : sec);
+    }
+
+    public static boolean isToday(Date time) {
+        Calendar before = Calendar.getInstance();
+        before.setTime(time);
+        int beforeYear = before.get(Calendar.YEAR);
+        int beforeDay = before.get(Calendar.DAY_OF_MONTH);
+        Calendar now = Calendar.getInstance();
+        int nowYear = now.get(Calendar.YEAR);
+        int nowDay = now.get(Calendar.DAY_OF_MONTH);
+
+        if(beforeYear == nowYear && beforeDay == nowDay) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String getTodayStr() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(calendar.getTime());
+    }
+
+    public static long getTodaySeconds() {
+        long current = System.currentTimeMillis();// 当前时间毫秒数
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        long todayzero = calendar.getTimeInMillis();
+        return (current - todayzero) / 1000;
+    }
+
+    public static long getTommorrowSeconds() {
+        long current = System.currentTimeMillis();// 当前时间毫秒数
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        long todayzero = calendar.getTimeInMillis();
+        return (current - todayzero) / 1000;
+    }
+
+    public static String getTommorrowStr() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(calendar.getTime());
     }
 }
